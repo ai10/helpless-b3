a9d93a933cee434a822d80f66a7a4f0063b8ae3a
Charles J. Short <short.chuck@gmail.com>
2013-10-06 13:38:50 -0400

    deleted:    bootstrap-3-alert.coffee 	deleted:    bs3alert.html 	modified:   package.js

--- package.js
+++ package.js
@@ -1,10 +1,12 @@
 Package.describe({
-    summary: "Help injecting bootstrap-3 alerts, ..."
+    summary: "Bootstrap 3 with Meteor style Javascripts., ..."
 });
 
 Package.on_use(function (api) {
-    api.use(['deps', 'templating', 'underscore', 'jquery', 'coffeescript']);
+    api.use(['deps', 'templating', 'less', 'underscore', 'jquery', 'coffeescript']);
 
-    api.add_files(['bs3alert.html', 'bootstrap-3-alert.coffee'], ['client']);
+    api.add_files([
+    'bootstrap.lessimport',
+    'b3alert.html', 'b3alert.coffee', 'b3.less'], ['client']);
 });
 
